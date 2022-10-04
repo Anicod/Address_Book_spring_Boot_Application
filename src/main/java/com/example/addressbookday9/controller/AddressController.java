6aef940f9ad1e75365597accb2f6c6d4a0907f49
Anicod <animeshraj735@gmail.com>
2022-10-04 22:47:02 +0530

    added: message

--- src/main/java/com/example/addressbookday9/controller/AddressController.java
+++ src/main/java/com/example/addressbookday9/controller/AddressController.java
@@ -1,8 +1,14 @@
 package com.example.addressbookday9.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AddressController {
+    @GetMapping("/get")
+    public String getMessage(){
+        return  "Welcome to address book program";
+    }
 
 }
