e29fb2a7eaeece3f329de247bac7cda9a3bf99f0
Anicod <animeshraj735@gmail.com>
2022-10-05 01:41:20 +0530

    added: all http request method

--- src/main/java/com/example/addressbookday9/controller/AddressController.java
+++ src/main/java/com/example/addressbookday9/controller/AddressController.java
@@ -1,9 +1,9 @@
 package com.example.addressbookday9.controller;
 
 import com.example.addressbookday9.model.AddressModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AddressController {
@@ -11,6 +11,27 @@ public class AddressController {
     public String getMessage(){
         AddressModel addressModel = new AddressModel("animesh", "Raj");
         return "welcome"+" "+addressModel.getFirstName()+" "+addressModel.getLastName();
+    }
+    //uc2
+    @GetMapping("/getbyinput")
+    ResponseEntity<AddressModel> getmessagebyinput(@RequestParam Integer Id, String firstName, String lastName, String address, String city, String state, Long zipCod, String note){
+        AddressModel addressModel = new AddressModel(Id, firstName, lastName, address, city, state, zipCod, note);
+        return new ResponseEntity<>(addressModel, HttpStatus.OK);
+    }
+    @GetMapping("getbyid/{Id}")
+    ResponseEntity<AddressModel> getById(@PathVariable Integer Id, @RequestParam String firstName, String lastName, String address, String city, String state, Long zipCod, String note ){
+        AddressModel addressModel = new AddressModel(Id, firstName, lastName, address, city, state, zipCod, note);
+        return new ResponseEntity<>(addressModel, HttpStatus.OK);
+    }
+    @PutMapping("/put/{firstName}")
+    ResponseEntity<AddressModel> putbyname(@PathVariable String firstName, @RequestParam Integer Id, String lastName, String address, String city, String state, Long zipCod, String note){
+        AddressModel addressModel = new AddressModel(Id, firstName, lastName, address, city, state, zipCod, note);
+        return new ResponseEntity<>(addressModel, HttpStatus.OK);
+    }
+    @PostMapping("/post")
+    ResponseEntity<AddressModel> postdata(@RequestBody AddressModel addressModel){
+        AddressModel addressModel1 = new AddressModel(addressModel.getId(), addressModel.getFirstName(), addressModel.getLastName(), addressModel.getAddress(), addressModel.getCity(), addressModel.getState(), addressModel.getZipCode(), addressModel.getNote());
+        return new ResponseEntity<>(addressModel1, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/addressbookday9/model/AddressModel.java
+++ src/main/java/com/example/addressbookday9/model/AddressModel.java
@@ -10,14 +10,31 @@ public class AddressModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer Id;
-    public String FirstName;
+    public String firstName;
     public String lastName;
+    public String address;
+    public String city;
+    public String state;
+    public Long zipCode;
+    public String note;
 
-    public AddressModel(String firstName, String lastName) {
-        FirstName = firstName;
+    public AddressModel(Integer id, String firstName, String lastName, String address, String city, String state, Long zipCode, String note) {
+        Id = id;
+        this.firstName = firstName;
         this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.note = note;
     }
+
     public AddressModel(){
+
+    }
+    public AddressModel(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
 
     }
 
@@ -30,11 +47,11 @@ public class AddressModel {
     }
 
     public String getFirstName() {
-        return FirstName;
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
-        FirstName = firstName;
+        this.firstName = firstName;
     }
 
     public String getLastName() {
@@ -43,5 +60,45 @@ public class AddressModel {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Long zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
     }
 }
