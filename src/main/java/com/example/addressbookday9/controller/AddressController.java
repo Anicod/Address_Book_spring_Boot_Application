5d4b73ef91ce29c8494342b6c2f1b46f654b96f8
Anicod <animeshraj735@gmail.com>
2022-10-05 23:11:23 +0530

    added: validation for all varibles

--- src/main/java/com/example/addressbookday9/controller/AddressController.java
+++ src/main/java/com/example/addressbookday9/controller/AddressController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +20,7 @@ public class AddressController {
     IAddressServe iAddressServe;
 
     @PostMapping("/postdtobyserviclayer")
-    ResponseEntity<AddressResponseDto> postDtoByServiceLayer(@RequestBody AddressDto addressDto){
+    ResponseEntity<AddressResponseDto> postDtoByServiceLayer(@Valid @RequestBody AddressDto addressDto){
        AddressModel addressModel = iAddressServe.save(addressDto);
        AddressResponseDto addressResponseDto = new AddressResponseDto(addressModel, "data posted successfully in database");
        return new ResponseEntity<>(addressResponseDto, HttpStatus.OK);
--- src/main/java/com/example/addressbookday9/dto/AddressDto.java
+++ src/main/java/com/example/addressbookday9/dto/AddressDto.java
@@ -2,14 +2,29 @@ package com.example.addressbookday9.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class AddressDto {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "name is invalid")
     public String firstName;
+    @Pattern(regexp = "^[a-zA-Z\\s]{2,}$", message = "last name is invalid")
     public String lastName;
+    @NotEmpty(message = "address not empty")
+    @Pattern(regexp = "^[a-zA-Z\\s]{2,}$", message = "address is invalid")
     public String address;
+    @NotEmpty(message = "city not empty")
+    @Pattern(regexp = "^[a-zA-Z\\s]{2,}$", message = "city is invalid")
     public String city;
+    @NotEmpty(message = "state not empty")
+    @Pattern(regexp = "^[a-zA-Z\\s]{2,}$", message = "state is invalid")
     public String state;
+    @NotNull(message = "zip code is requried")
+    @Min(value = 10000, message = "pin code is not valid")
+    @Max(value = 99999, message = "not greter than 4 digit")
     public Long zipCode;
+    @NotEmpty(message = "state not empty")
+    @Pattern(regexp = "^[a-zA-Z\\s]{2,}$", message = "note is invalid")
     public String note;
 
     public AddressDto( String firstName, String lastName, String address, String city, String state, Long zipCode, String note) {
